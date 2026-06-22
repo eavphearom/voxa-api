a62c47decb9b585aa61312767385f6cb41d80184
Phearom777 <eavphearom168@gmail.com>
2026-06-23 00:17:20 +0700

    add celery and redis

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.http import JsonResponse
 
+
+def home(request):
+    return JsonResponse({
+        "success": True,
+        "message": "Voxa API is running"
+    })
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("app.Routes.api")),
 ]
