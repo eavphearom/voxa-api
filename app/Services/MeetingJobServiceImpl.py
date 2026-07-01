e02198a5d3e8c83c0f7ead1da18edc92af6e30e4
Phearom777 <eavphearom168@gmail.com>
2026-07-01 10:55:27 +0700

    fix error on production

--- app/Services/MeetingJobServiceImpl.py
+++ app/Services/MeetingJobServiceImpl.py
@@ -1,5 +1,7 @@
 import logging
 
+from celery import app
+
 from app.Exceptions import ApplicationException
 from app.Services.Contracts.MeetingJobService import MeetingJobService
 
@@ -9,12 +11,13 @@ logger = logging.getLogger(__name__)
 class MeetingJobServiceImpl(MeetingJobService):
 
     def enqueue(self, meeting_id: int, user_id: int) -> str:
-        logger.error("AAAAAAAAAAAA ENQUEUE CALLED")
+        logger.error(" ENQUEUE CALLED")
         from app.tasks import process_meeting_transcription
-
+        from core.celery import app
         try:
             logger.info("BEFORE DELAY")
-
+            print("BROKER =", app.conf.broker_url)
+            print("BACKEND =", app.conf.result_backend)
             task = process_meeting_transcription.delay(meeting_id, user_id)
 
             logger.info(f"TASK ID: {task.id}")
